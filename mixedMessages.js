ee80f67fd0b10ea660b61035f5dbcc4ebd16712a
Tomasz Dyngosz <tom.dyngosz@gmail.com>
2021-12-08 20:56:52 +0100

    Create the message

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,3 +1,18 @@
 const names = ['idiot', 'son of a bitch', 'asshole', 'weirdo', 'rapist', 'motherfucker', 'fucker', 'cunt']
 
+const selectRandom = (arr) => {
+
+    const randomNames = []
+
+    while (randomNames.length < 3) {
+        const random = Math.floor(Math.random() * arr.length)
+
+        if (randomNames.every(element => element !== arr[random]))
+            randomNames.push(arr[random])
+        else
+            continue
+    }
+}
+
+selectRandom(names)
 
